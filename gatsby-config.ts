36ebba6628c1c1604de602ea426d3bfa9ac66b7e
vijayaraj <Vijayfullstackdeveloper@gmail.com>
2023-10-06 00:11:29 +0400

    installed Typescript and changed all files to .tsx and .ts

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,8 +1,9 @@
 module.exports = {
+  pathPrefix: `/Portfolio`,
   siteMetadata: {
-    title: `Gatsby Boostrap 5 Sass Starter`,
-    description: `A simple bootstrap 5 and Sass starter for Gatsby. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@r-ichard`,
+    title: `Vijayaraj's Portfolio`,
+    description: `project i worked, technical skills, license, certification, about me, companies.`,
+    author: `vijayaraj suyambu`,
   },
   plugins: [
     `gatsby-plugin-image`,
